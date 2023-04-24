1c7f5a89c3bcdfcf7d22c38e6ede994770c2726d
Alaa-M1 <alaa85a@gmail.com>
2023-04-24 21:44:45 +0200

    add Google and Github authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session1, ...pageProps1 },
+}: AppProps) {
+  return (
+    <SessionProvider session={session1}>
+      <Component {...pageProps1} />
+    </SessionProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log('session data=',session)
   return (
-    <>
-      <Head>
-        <title>NextJs Authentication App</title>
-        <meta name="description" content="Authentication App with NextJs" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div>
-          Authentication App with NextJs
-        </div>
-      </main>
-    </>
-  )
+    <main style={{ height: "100vh"}}>
+      <Box display="flex" justifyContent="flex-start" m={2}>
+        {session ? (
+          <>
+            <Box component="span" margin={"0px 15px 0px 5px"}>Signed in as {session?.user?.email}:</Box>
+            <Button variant="contained" color="primary" onClick={() => signOut()}>
+              Sign out
+            </Button>
+          </>
+        ) : (
+          <>
+            <Box component="span" margin={"0px 15px 0px 5px"}>Not signed in </Box>
+            <Button variant="contained" color="primary" onClick={() => signIn()}>
+              Sign in
+            </Button>
+          </>
+        )}
+      </Box>{" "}
+    </main>
+  );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  console.log('server session=',session)
+  return {
+    props: {
+      session,
+  }
+}
 }
