a6c39f5ac80ae699dce13465ae4df515e6637644
Alaa-M1 <alaa85a@gmail.com>
2023-04-26 17:26:51 +0200

    add validations for the user form

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -4,84 +4,146 @@ import PersonIcon from "@mui/icons-material/Person";
 import BusinessIcon from "@mui/icons-material/Business";
 import EmailIcon from "@mui/icons-material/Email";
 import SmartphoneIcon from "@mui/icons-material/Smartphone";
-import LockIcon from '@mui/icons-material/Lock';
+import LockIcon from "@mui/icons-material/Lock";
 
 import { ChangeEvent, useState } from "react";
-const SignUp = () => {
-  const [userInfo, setUserInfo] = useState({
-    firstName: "",
-    lastName: "",
-    address: "",
-    email: "",
-    mobile: "",
-    password: "",
-    confirmPassword: "",
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import validator from "validator";
+
+const UserSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(5, "The first name must be at least 5 characters")
+      .max(32, "The first name must be less than 32 characters")
+      .regex(
+        new RegExp("^[a-zA-Z]+$"),
+        "The first name must not contains any special characters"
+      ),
+    lastName: z
+      .string()
+      .min(5, "The last name must be at least 5 characters")
+      .max(32, "The last name must be less than 32 characters")
+      .regex(
+        new RegExp("^[a-zA-Z]+$"),
+        "The last name must not contains any special characters"
+      ),
+    address: z
+      .string()
+      .min(8, "The address must be at least 8 characters")
+      .max(100, "The address must be less than 100 characters"),
+    email: z.string().email("You must enter a valid Email"),
+    mobile: z.string().refine(validator.isMobilePhone, {
+      message: "Please enter a valid phone number",
+    }),
+    password: z
+      .string()
+      .min(8, "The password must be at least 8 characters")
+      .max(60, "The password must be less than 60 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((formData) => formData.password === formData.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
   });
-  console.log("userInfo=", userInfo);
-  const handleChnage = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserInfo((p) => ({ ...p, [e.target.name]: e.target.value }));
-  };
+
+type UserSchemaType = z.infer<typeof UserSchema>;
+
+const SignUp = () => {
+  // const [userInfo, setUserInfo] = useState({
+  //   firstName: "",
+  //   lastName: "",
+  //   address: "",
+  //   email: "",
+  //   mobile: "",
+  //   password: "",
+  //   confirmPassword: "",
+  // });
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<UserSchemaType>({ resolver: zodResolver(UserSchema) });
+  const onSubmit = (data: any) => console.log(data);
+
+  // console.log("userInfo=", userInfo);
+  // const handleChnage = (e: ChangeEvent<HTMLInputElement>) => {
+  //   setUserInfo((p) => ({ ...p, [e.target.name]: e.target.value }));
+  // };
   return (
     <Box>
-        <Typography variant="h4" color="primary">Sign Up</Typography>
-      <form action="">
+      <Typography variant="h4" color="primary">
+        Sign Up
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <TextField
-          value={userInfo.firstName}
           name="firstName"
           label="First Name"
           placeholder="First name"
           icon={<PersonIcon />}
-          onChange={handleChnage}
+          register={register}
+          errors={errors.firstName?.message}
+          disabled={isSubmitting}
         ></TextField>
         <TextField
-          value={userInfo.lastName}
           name="lastName"
           label="Last Name"
           placeholder="Last name"
           icon={<PersonIcon />}
-          onChange={handleChnage}
+          register={register}
+          errors={errors.lastName?.message}
+          disabled={isSubmitting}
         ></TextField>
         <TextField
-          value={userInfo.address}
           name="address"
           label="Address"
           placeholder="Address"
           icon={<BusinessIcon />}
-          onChange={handleChnage}
+          register={register}
+          errors={errors.address?.message}
+          disabled={isSubmitting}
         ></TextField>
         <TextField
-          value={userInfo.email}
           name="email"
           label="Email"
           placeholder="Email"
           icon={<EmailIcon />}
-          onChange={handleChnage}
+          register={register}
+          errors={errors.email?.message}
+          disabled={isSubmitting}
         ></TextField>
         <TextField
-          value={userInfo.mobile}
           name="mobile"
           label="Mobile number"
           placeholder="Mobile number"
           icon={<SmartphoneIcon />}
-          onChange={handleChnage}
+          register={register}
+          errors={errors.mobile?.message}
+          disabled={isSubmitting}
         ></TextField>
         <TextField
-          value={userInfo.password}
           name="password"
           label="Password"
           placeholder=""
           icon={<LockIcon />}
           type="password"
-          onChange={handleChnage}
+          register={register}
+          errors={errors.password?.message}
+          disabled={isSubmitting}
         ></TextField>
         <TextField
-          value={userInfo.confirmPassword}
           name="confirmPassword"
           label="Confirm Password"
           placeholder=""
           icon={<LockIcon />}
           type="password"
-          onChange={handleChnage}
+          register={register}
+          errors={errors.confirmPassword?.message}
+          disabled={isSubmitting}
         ></TextField>
 
         <Button variant="contained" color="primary" type="submit">
--- src/pages/components/TextField.tsx
+++ src/pages/components/TextField.tsx
@@ -1,66 +1,102 @@
-import { Box } from "@mui/material";
+import { Alert, Box } from "@mui/material";
 import {
   ChangeEvent,
   DetailedHTMLProps,
   InputHTMLAttributes,
   useId,
 } from "react";
+import ErrorIcon from "@mui/icons-material/Error";
 
 type TextFieldProps = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 > & {
   label: string;
-  name: string;
-  value: string | number | readonly string[] | undefined;
-  placeholder: string;
   icon: JSX.Element;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  register: any;
+  errors: any;
+  //   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 const TextField = ({
-  label,
   name,
-  value,
-  placeholder,
+  label,
   icon,
-  onChange,
+  register,
+  errors,
+  //   onChange,
   ...props
 }: TextFieldProps) => {
   const id = useId();
   return (
     <Box
       sx={{
-        position: "relative",
         width: "auto",
-        border: "1px solid #0af",
-        borderRadius: "6px",
         padding: "0px 10px",
-        textAlign: "left",
         margin: "10px 5px",
       }}
     >
       <Box
         sx={{
-          position: "absolute",
-          bottom: "0px",
-          left: "10px",
-          svg: { padding: "0px", marginBottom: "-5px" },
+          position: "relative",
+          width: "auto",
+          textAlign: "left",
+          ">input:focus": {
+            borderWidth: "1px",
+            borderColor: errors ? "#d32f2f" : "#ccc",
+            outlineColor: errors ? "#d32f2f" : "#ccc",
+          },
         }}
       >
-        {icon}
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "4px",
+            left: "5px",
+            svg: { padding: "0px", margin: "0px 0px 3px 5px" },
+          }}
+        >
+          {icon}
+        </Box>
+        <label htmlFor={`input-${id}`} style={{ marginLeft: "5px" }}>
+          {label}
+        </label>
+        <br />
+        <input
+          id={`input-${id}`}
+          type="text"
+          name={name}
+          // onChange={onChange}
+          {...register(name)}
+          className="custom-input"
+          {...props}
+          style={{
+            padding: "8px 5px 8px 30px",
+            margin: "5px",
+            borderWidth: "1px",
+            borderColor: errors ? "#d32f2f" : "#ccc",
+            borderRadius: "6px",
+            width: "100%",
+            boxSizing: "border-box",
+          }}
+        />
+        {errors && (
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "4px",
+              right: "10px",
+              svg: {
+                padding: "0px",
+                margin: "0px 0px 3px 5px",
+                color: "#d32f2f",
+              },
+            }}
+          >
+            <ErrorIcon />
+          </Box>
+        )}
       </Box>
-      <label htmlFor={`input-${id}`}>{label}</label>
-      <br />
-      <input
-        name={name}
-        value={value}
-        id={`input-${id}`}
-        type="text"
-        placeholder={placeholder}
-        onChange={onChange}
-        className="custom-input"
-        {...props}
-      />
+      {errors && <Alert severity="error">{errors}</Alert>}
     </Box>
   );
 };
